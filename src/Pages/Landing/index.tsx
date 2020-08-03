b5f4e63f18bd436e68850986b0cdff87801209f9
Thiago Demas <thiago.demas7@gmail.com>
2020-08-03 16:03:46 -0300

    Criação da Pagina Landing

---
+++ src/Pages/Landing/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+import LogoImg from "../../assets/images/logo.svg";
+import LandingImg from "../../assets/images/landing.svg";
+
+import StudyIcon from "../../assets/images/icons/study.svg";
+import GiveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import PurpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+
+const Landing: React.FC = () => {
+  return (
+    <div id="page-landing">
+      <div className="container" id="page-landing-content">
+        <div className="logo-container">
+          <img src={LogoImg} alt="Proffy" />
+          <h2>Sua plataforma de estudos online.</h2>
+        </div>
+        <img
+          src={LandingImg}
+          alt="Plataforma de Estudos"
+          className="hero-image"
+        />
+
+        <div className="buttons-container">
+            <a href="" className="study">
+                <img src={StudyIcon} alt="Study Icon"/>
+                Estudar
+            </a>
+            <a href="" className="give-classes">
+                <img src={GiveClassesIcon} alt="Study Icon"/>
+                Estudar
+            </a>
+        </div>
+
+        <span className="total-connections">
+            Total de 200 conexões já realizadas <img src={PurpleHeartIcon} alt="Heart"/>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
